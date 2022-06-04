bcf2dbe3f3647c1f5832254e0a3769c97bab575d
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2022-06-04 12:43:16 +0300

    refactor: change structure of the project

--- pages/main/script.js
+++ pages/main/script.js
@@ -1,12 +1,2 @@
-const BURGER = document.querySelector('.burger');
-const burgerItem = document.querySelector('.burger__item');
-const burgerMenu = document.querySelector('.burger__menu');
-
-// burger.classList.toggle('burger_active')
-
-const openBurger = () => {
-  burgerItem.classList.add('burger__item_rotate');
-  burgerMenu.classList.add('burger__menu_active');
-}
-
-BURGER.addEventListener('click', openBurger);
+import { openBurger } from "./js/burger.js";
+import { closeBurger } from "./js/burger.js";
