4d6c82749c1ae1f424f35e884d98a3940d859be1
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2022-06-06 11:48:03 +0300

    refactor: add import

--- pages/main/script.js
+++ pages/main/script.js
@@ -3,3 +3,23 @@ import { closeBurger } from "./js/burger.js";
 
 openBurger();
 closeBurger();
+
+
+let offset = 0;
+const cardsSlider = document.querySelector('.cards__slider');
+
+document.querySelector('#next').addEventListener('click', function() {
+  offset = offset + 270;
+  if (offset > 1200) {
+    offset = 0;
+  }
+  cardsSlider.style.left = -offset + 'px';
+});
+
+document.querySelector('#prev').addEventListener('click', function() {
+  offset = offset - 270;
+  if (offset < 0) {
+    offset = 1200;
+  }
+  cardsSlider.style.left = -offset + 'px';
+});
