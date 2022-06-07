5cc967880bd593306397deb70d3f24d2fa1536e2
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2022-06-07 21:24:29 +0300

    feat: add pop-up

--- pages/main/script.js
+++ pages/main/script.js
@@ -4,7 +4,7 @@ import { closeBurger } from "./js/burger.js";
 openBurger();
 closeBurger();
 
-
+// slider
 let offset = 0;
 const cardsSlider = document.querySelector('.cards__slider');
 
@@ -23,3 +23,21 @@ document.querySelector('#prev').addEventListener('click', function() {
   }
   cardsSlider.style.left = -offset + 'px';
 });
+
+
+// pop-up
+
+const card = document.querySelector('.card');
+const popUp = document.querySelector('.pop-up');
+const popUpBtnClose = document.querySelector('.pop-up__btn-close');
+
+
+card.addEventListener('click', () => {
+  popUp.classList.toggle('pop-up_active');
+  // document.getElementsByTagName('body').classList.add()
+})
+
+popUpBtnClose.addEventListener('click', () => {
+  popUp.classList.toggle('pop-up_active');
+})
+
